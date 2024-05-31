fc0ce902ddd28341a64af0e65fd71ce552c99ee4
ste <mrbarrow@hotmail.co.uk>
2024-05-31 10:34:05 +0100

    initial commit

---
+++ src/main/java/com/northcoderse/drinksapi/DrinksapiApplication.java
@@ -0,0 +1,13 @@
+package com.northcoderse.drinksapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinksapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinksapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northcoderse/drinksapi/DrinksapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoderse.drinksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinksapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
